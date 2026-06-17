ab3e1f01f2645652a143c30145ec117d6b8b5c4e
Madhura Shende <madhu5shende@gmail.com>
2026-06-17 22:50:01 +0530

    Add files via upload

---
+++ constant.c
@@ -0,0 +1,15 @@
+/*
+const Keyword:
+- Used to declare a constant variable.
+- Value cannot be changed after initialization.
+- Improves code safety and readability.
+- Example: const int PI = 3;
+*/
+
+#include<stdio.h>
+int main() 
+{
+    const int PI=3;
+    printf("PI=%d",PI);
+    return 0;
+}
