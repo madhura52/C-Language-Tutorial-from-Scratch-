acca0e3f2e93aa70921e5341548201d840f6e213
Madhura Shende <madhu5shende@gmail.com>
2026-06-23 11:42:15 +0530

    Add files via upload
    add simple input output code file

---
+++ input_output.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() 
+{
+    int number;   // Variable to store the user's input
+    printf("Enter a number:");   // Ask the user for input
+    scanf("%d",&number);         // Read the input from the keyboard
+    printf("You entered:%d",number);   // Display the entered number
+    return 0;   // End of the program
+}
