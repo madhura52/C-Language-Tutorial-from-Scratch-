d4669048592e5d790bff70663772fba3a4d3616c
Madhura Shende <madhu5shende@gmail.com>
2026-06-30 12:20:45 +0530

    Update bin_ass.c

--- bin_ass.c
+++ bin_ass.c
@@ -1,16 +1,16 @@
 // //Bitwise AND
-// #include<stdio.h>
-// int main() 
-// {
-//     int a,b;
-//     printf("Enter two numbers: ");
-//     scanf("%d %d",&a,&b);
+#include<stdio.h>
+int main() 
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
 
-//     int result=a&b;
+    int result=a&b;
 
-//     printf("Result(a&b)=%d\n",result);
-//     return 0;
-// }
+    printf("Result(a&b)=%d\n",result);
+    return 0;
+}
 
 // //Bitwise OR
 // #include<stdio.h>
@@ -25,28 +25,28 @@
 // }
 
 // //Bitwise XOR
-// #include <stdio.h>
-// int main() 
-// {
-//     int a,b;
-//     printf("Enter two numbers:");
-//     scanf("%d %d",&a,&b);
-//     int result=a^b;
-//     printf("Result(a^b)=%d\n",result);
-//     return 0;
-// }
+#include <stdio.h>
+int main() 
+{
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    int result=a^b;
+    printf("Result(a^b)=%d\n",result);
+    return 0;
+}
 
 // //Left Shift
-// #include <stdio.h>
-// int main() 
-// {
-//     int num;
-//     printf("Enter a number: ");
-//     scanf("%d",&num);
-//     int result=num<<1;
-//     printf("Result (num<<1)=%d\n",result);
-//     return 0;
-// }
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    int result=num<<1;
+    printf("Result (num<<1)=%d\n",result);
+    return 0;
+}
 
 // //Right shift
 // #include<stdio.h>
@@ -62,26 +62,26 @@
 
 
 //Toggle on/off
-// #include <stdio.h>
-// int main() {
-//     int state;
-//     printf("Enter current state (0 or 1): ");
-//     scanf("%d", &state);
-//     state = state ^ 1;
-//     printf("New state: %d\n", state);
-//     return 0;
-// }
+#include <stdio.h>
+int main() {
+    int state;
+    printf("Enter current state (0 or 1): ");
+    scanf("%d", &state);
+    state = state ^ 1;
+    printf("New state: %d\n", state);
+    return 0;
+}
 
 //double score
-// #include <stdio.h>
-// int main() {
-//     int score;
-//     printf("Enter score: ");
-//     scanf("%d", &score);
-//     int result = score << 1;
-//     printf("Doubled score: %d\n", result);
-//     return 0;
-// }
+#include <stdio.h>
+int main() {
+    int score;
+    printf("Enter score: ");
+    scanf("%d", &score);
+    int result = score << 1;
+    printf("Doubled score: %d\n", result);
+    return 0;
+}
 
 //half value(right shift)
 // #include <stdio.h>
@@ -107,15 +107,15 @@ int main() {
 }
 
 //find differnce of number using assignment operator
-// #include <stdio.h>
-// int main() {
-//     int a, b;
-//     printf("Enter two numbers: ");
-//     scanf("%d %d", &a, &b);
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
 
-//     int result = a ^ b;
+    int result = a ^ b;
 
-//     printf("Difference (XOR): %d\n", result);
+    printf("Difference (XOR): %d\n", result);
 
-//     return 0;
-// }
+    return 0;
+}
